830309b08e79acfb95eb66fce0ee4aa16a79c821
Hendra Permana <hynra@yahoo.com>
2017-01-05 10:05:09 +0700

    Add mq config

---
+++ app/src/main/java/project/bsts/semut/connections/broker/Config.java
@@ -0,0 +1,13 @@
+package project.bsts.semut.connections.broker;
+
+import project.bsts.semut.setup.Constants;
+
+public class Config {
+    public static final String hostName = Constants.MQ_HOSTNAME;
+    public static final String virtualHostname = Constants.MQ_VIRTUAL_HOST;
+    public static final String username = Constants.MQ_USERNAME;
+    public static final String password = Constants.MQ_PASSWORD;
+    public static final int port = Constants.MQ_PORT;
+    public static final String exchange = Constants.MQ_EXCHANGE_NAME;
+    public static final String rotuingkey = Constants.MQ_DEFAULT_ROUTING_KEY;
+}
