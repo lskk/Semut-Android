f8f12771a74500c1efe78e32c3fa04a1e2e99891
Hendra Permana <hynra@yahoo.com>
2017-02-10 14:20:20 +0700

    butterknife

--- app/src/main/java/project/bsts/semut/LoginActivity.java
+++ app/src/main/java/project/bsts/semut/LoginActivity.java
@@ -3,14 +3,34 @@ package project.bsts.semut;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.fb_Login)
+    Button fbLoginBtn;
+    @BindView(R.id.login_btn)
+    Button loginBtn;
+    @BindView(R.id.email)
+    EditText emailEditText;
+    @BindView(R.id.password)
+    EditText passEditText;
+    @BindView(R.id.skip_btn)
+    Button skipBtn;
+    @BindView(R.id.signup_btn)
+    Button signupBtn;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
     }
 }
