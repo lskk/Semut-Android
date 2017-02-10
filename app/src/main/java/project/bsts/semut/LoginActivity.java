f790bfc3087dccda405323e82fda8c3cca5b1e7d
Hendra Permana <hynra@yahoo.com>
2017-02-10 16:24:51 +0700

    Add facebook login

--- app/src/main/java/project/bsts/semut/LoginActivity.java
+++ app/src/main/java/project/bsts/semut/LoginActivity.java
@@ -1,18 +1,36 @@
 package project.bsts.semut;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @BindView(R.id.fb_Login)
-    Button fbLoginBtn;
+ //   @BindView(R.id.loginButton)
+ //   LoginButton fbLoginBtn;
     @BindView(R.id.login_btn)
     Button loginBtn;
     @BindView(R.id.email)
@@ -26,11 +44,60 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    CallbackManager callbackManager;
+    LoginButton fbLoginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginBtn = (LoginButton)findViewById(R.id.loginButton);
+
+        fbLoginBtn.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
+        fbLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    Log.i("FB", String.valueOf(object));
+                                    Log.i("FB", response.getRawResponse());
+                                    String name=object.getString("name");
+                                    String email=object.getString("email");
+                                    Log.i("Login Success : ", name+", "+email);
+                                } catch(JSONException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
